962b9edd35269142e7290f44332a428b341abac4
ramshadvp <ramshad.vp@innovaturelabs.com>
2023-01-10 10:57:55 +0530

    fgt

--- src/main/java/com/example/New_Demo/NewDemoApplication.java
+++ src/main/java/com/example/New_Demo/NewDemoApplication.java
@@ -2,12 +2,14 @@ package com.example.New_Demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NewDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewDemoApplication.class, args);
+		new SpringApplicationBuilder(NewDemoApplication.class).run().close();
 	}
 
 }
